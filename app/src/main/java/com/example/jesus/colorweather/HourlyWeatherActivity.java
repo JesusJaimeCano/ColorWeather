b35e9d6424ef702f2a4941f3dfd8ed10a7ee35ca
JCano <32400980+JesusJaimeCano@users.noreply.github.com>
2017-11-27 17:14:09 -0600

    Creacion de layouts
    Fin de modulo de layouts avanzados en android, justo antes de insertar los intents

---
+++ app/src/main/java/com/example/jesus/colorweather/HourlyWeatherActivity.java
@@ -0,0 +1,13 @@
+    package com.example.jesus.colorweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HourlyWeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hourly_weather);
+    }
+}
