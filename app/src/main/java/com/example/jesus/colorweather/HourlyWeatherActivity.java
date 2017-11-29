bf4c870c63a1922f5b46e215a746364ac216613c
JCano <32400980+JesusJaimeCano@users.noreply.github.com>
2017-11-28 19:48:36 -0600

    Modulo web services video 94

--- app/src/main/java/com/example/jesus/colorweather/HourlyWeatherActivity.java
+++ app/src/main/java/com/example/jesus/colorweather/HourlyWeatherActivity.java
@@ -1,13 +1,15 @@
     package com.example.jesus.colorweather;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HourlyWeatherActivity extends AppCompatActivity {
+public class HourlyWeatherActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_weather);
+
     }
 }
--- app/src/main/java/com/example/jesus/colorweather/MainActivity.java
+++ app/src/main/java/com/example/jesus/colorweather/MainActivity.java
@@ -1,21 +1,43 @@
 package com.example.jesus.colorweather;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import butterknife.BindDrawable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity {
+
+
+    @BindView(R.id.iconImageView)
+    ImageView iconImageView;
+    @BindView(R.id.descriptionTextView)
+    TextView descriptionTextView;
+    @BindView(R.id.currentTempTextView)
+    TextView currentTempTextView;
+    @BindView(R.id.highestTempTextView)
+    TextView highestTempTextView;
+    @BindView(R.id.lowestTempTextView)
+    TextView lowestTempTextView;
+
+    @BindDrawable(R.drawable.clear_night)
+    Drawable clearnight;
+
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +45,67 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+
+        CurrentWeather currentWeather = new CurrentWeather(MainActivity.this);
+
+        currentWeather.setIconImage("sunny");
+        currentWeather.setDescription("SunnyDay");
+        currentWeather.setCurrentTemperature("24");
+        currentWeather.setHighestTemperature("H: 25");
+        currentWeather.setLowestTemperature("L: 10");
+
+        iconImageView.setImageDrawable(currentWeather.getIconDrawableResource());
+        descriptionTextView.setText(currentWeather.getDescription());
+        currentTempTextView.setText(currentWeather.getCurrentTemperature());
+        highestTempTextView.setText(currentWeather.getHighestTemperature());
+        lowestTempTextView.setText(currentWeather.getLowestTemperature());
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.google.com";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+
+                        Log.d(TAG, "Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Log.d(TAG, "That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
+
     }
 
     @OnClick(R.id.dailyWeatherTextView)
-    public void dailyWeatherClick(){
+    public void dailyWeatherClick() {
 
-        Intent dailyActivityIntent =  new Intent(MainActivity.this, DailyWeatherActivity.class);
+        Intent dailyActivityIntent = new Intent(MainActivity.this, DailyWeatherActivity.class);
 
         startActivity(dailyActivityIntent);
     }
+
+    @OnClick(R.id.hourlyWeatherTextView)
+    public void hourlyWeatherClick() {
+        Intent hourlyActivityIntent = new Intent(MainActivity.this, HourlyWeatherActivity.class);
+
+        startActivity(hourlyActivityIntent);
+    }
+
+    @OnClick(R.id.minutelyWeatherTextView)
+    public void minutelyWeatherClick() {
+        Intent minutelyActivityIntent = new Intent(MainActivity.this, MinutelyWeatherActivity.class);
+
+        startActivity(minutelyActivityIntent);
+    }
+
 }
